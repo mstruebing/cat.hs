a5c7917e313912f260e6e20039fcdedcf21811ec
Max Strübing <mxstrbng@gmail.com>
2017-09-27 14:45:10 +0200

    refactor: move functions into lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,14 @@
 module Main where
 
-import Lib
+import Lib (printFile, printFileWithLineNumbers)
 import System.Environment (getArgs)
 
 main :: IO ()
-main = getArgs >>= mapM_ printFileWithLineNumbers
-
-printFile :: FilePath -> IO ()
-printFile filePath = readFile filePath >>= putStr
-
-printFileWithLineNumbers :: FilePath -> IO ()
-printFileWithLineNumbers filePath = readFile filePath >>= mapM_ (\x -> putStrLn $ (show $ fst x) ++ "\t" ++ snd x) . generateNumberedList
-
-generateNumberedList :: String -> [(Int, String)]
-generateNumberedList = zip [1 ..] . lines
+main = do
+    args <- getArgs
+    let lineNumbers = length (filter (== "-n") args) > 0
+    let files = filter (/= "-n") args
+    if lineNumbers then
+        mapM_ printFileWithLineNumbers files
+    else
+        mapM_ (\file -> printFile file) files
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
+    ( printFile
+    , printFileWithLineNumbers
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+printFile :: FilePath -> IO ()
+printFile filePath = readFile filePath >>= putStr
+
+printFileWithLineNumbers :: FilePath -> IO ()
+printFileWithLineNumbers filePath = readFile filePath >>= mapM_ (\x -> putStrLn $ (show $ fst x) ++ "\t" ++ snd x) . generateNumberedList
+
+generateNumberedList :: String -> [(Int, String)]
+generateNumberedList = zip [1 ..] . lines
