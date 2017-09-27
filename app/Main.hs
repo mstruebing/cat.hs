c144d681a5fc3b754f1eea1a41f70274471cbd5f
Max Strübing <mxstrbng@gmail.com>
2017-09-27 13:46:31 +0200

    feat: implement printFileWithLineNumbers

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,13 @@ module Main where
 import Lib
 
 main :: IO ()
-main = printFile "/home/maex/projects/own/cat/app/Main.hs"
+main = printFileWithLineNumbers "/home/maex/projects/own/cat/app/Main.hs"
 
 printFile :: FilePath -> IO ()
-printFile filePath = do 
-    content <- readFile filePath
-    putStr content
+printFile filePath = readFile filePath >>= putStr
+
+printFileWithLineNumbers :: FilePath -> IO ()
+printFileWithLineNumbers filePath = readFile filePath >>= mapM_ (\x -> putStrLn $ (show $ fst x) ++ "\t" ++ snd x) . generateNumberedList 
+
+generateNumberedList :: String -> [(Int, String)]
+generateNumberedList = zip [1 ..] . lines 
