be27eae91f4f461cd8419503e24e48e928f0c949
Max Strübing <mxstrbng@gmail.com>
2017-09-27 13:52:18 +0200

    chore: add editorconfig

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ printFile :: FilePath -> IO ()
 printFile filePath = readFile filePath >>= putStr
 
 printFileWithLineNumbers :: FilePath -> IO ()
-printFileWithLineNumbers filePath = readFile filePath >>= mapM_ (\x -> putStrLn $ (show $ fst x) ++ "\t" ++ snd x) . generateNumberedList 
+printFileWithLineNumbers filePath = readFile filePath >>= mapM_ (\x -> putStrLn $ (show $ fst x) ++ "\t" ++ snd x) . generateNumberedList
 
 generateNumberedList :: String -> [(Int, String)]
-generateNumberedList = zip [1 ..] . lines 
+generateNumberedList = zip [1 ..] . lines
